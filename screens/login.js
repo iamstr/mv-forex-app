ba3197eb29deafa9009dd57457a2612d38746681
iamsatar <asatar1994@gmail.com>
2022-12-09 03:45:57 +0300

    chore: project start

---
+++ screens/login.js
@@ -0,0 +1,51 @@
+// In the React Native app
+import React, { useState } from 'react';
+import { Button, Text, View } from 'react-native';
+
+const App = () => {
+  const [jwt, setJWT] = useState(null); // JWT state
+
+  const login = () => {
+    // Send a login request to the Node.js server
+    fetch('https://example.com/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: 'user1',
+        password: 'password123'
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Save the JWT locally, such as in the device's local storage
+        saveJWT(data.token);
+        setJWT(data.token);
+      });
+  };
+
+  const getProtectedData = () => {
+    // Make a request to the protected route on the Node.js server
+    const jwt = getJWT(); // Retrieve the JWT from local storage
+    fetch('https://example.com/protected-route', {
+      headers: {
+        Authorization: `Bearer ${jwt}`
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response data
+      });
+  };
+
+  return (
+    <View>
+      {jwt ? (
+        <>
+          <Text>Welcome, user!</Text>
+          <Button title="Get Protected Data" onPress={getProtectedData} />
+        </>
+      ) : (
+        <Button title="Login" onPress={login} />
+      )}
+    </View>
+  );
+};
