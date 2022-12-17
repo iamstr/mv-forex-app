5e9313decb53fdacda38d78a497ba458e5039667
iamsatar <asatar1994@gmail.com>
2022-12-17 18:47:54 +0300

    chore:add tailwind config

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,10 @@
 // In the React Native app
 import React, { useState } from 'react';
-import { Button, Text, View } from 'react-native';
-
+import { Button, Text, View,SafeAreaView,ImageBackground } from 'react-native';
+import {useTailwind} from 'tailwind-rn';
 export default function LoginScreen ()  {
   const [jwt, setJWT] = useState(null); // JWT state
-
+const tailwind = useTailwind();
   const login = () => {
     // Send a login request to the Node.js server
     fetch('https://example.com/login', {
@@ -42,9 +42,18 @@ export default function LoginScreen ()  {
       {jwt ? (
         getProtectedData()
       ) : (
-        <View>
-        <Button title="Login" onPress={login} />
-        </View>
+
+
+        	<SafeAreaView style={tailwind('h-full')}>
+    <ImageBackground source={require('./assets/pattern_japanese-pattern-3_1_2_0-0_0_1__4bfb9d_ffffff@2x.png')} style={{width: '100%', height: '100%',position:'relative'}}>
+		<View style={tailwind('position-absolute z-10 inset-0 bg-white rounded-r-3xl rounded-l-3xl')}>
+		 <Button title="Login" onPress={login} />
+		</View>
+    </ImageBackground>
+	</SafeAreaView>
+      
+       
+        
       )}
     </View>
   );
