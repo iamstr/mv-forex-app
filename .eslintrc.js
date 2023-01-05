1363ad0f0cf3a71681f8f8a62f04397e9e2787cd
iamsatar <asatar1994@gmail.com>
2023-01-05 19:13:32 +0300

    chore: change double  to single qoutes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,36 +2,29 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    "react-native/react-native": true,
+    'react-native/react-native': true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    "react-native"
-  ],
+  plugins: ['react', 'react-native'],
   rules: {
-     "react-native/no-unused-styles": 2,
-    "react-native/split-platform-components": 2,
-    "react-native/no-inline-styles": 2,
-    "react-native/no-color-literals": 2,
-    "react-native/no-raw-text": 2,
-    
-    "react-native/sort-styles": [
-      "error",
-      "asc",
-       {
-         "ignoreClassNames": false,
-         "ignoreStyleProperties": false
-       }
-     ]
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+
+    'react-native/sort-styles': [
+      'error',
+      'asc',
+      {
+        ignoreClassNames: false,
+        ignoreStyleProperties: false,
+      },
+    ],
   },
 };
