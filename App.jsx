d942a9c825b4fec00efee02f905ab85e40049101
iamsatar <asatar1994@gmail.com>
2023-01-09 00:50:22 +0300

    feat: add deposit, channel screen to the stack add close button for the modal

--- App.jsx
+++ App.jsx
@@ -1,10 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Pressable, StyleSheet } from 'react-native';
 import { RootSiblingParent } from 'react-native-root-siblings';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import CustomTab from './CustomTab';
 import ConfirmAccountScreen from './screens/ConfirmAccountScreen';
+import { default as ChannelScreen, default as DepositScreen } from './screens/DepositScreen';
 // import LoginScreen from "./screens/LoginScreen"
+import _themeColor from './colorScheme.json';
 import ForgotScreen from './screens/ForgotScreen';
 import LoginScreen from './screens/LoginScreen';
 import NotificationScreen from './screens/NotificationScreen';
@@ -16,6 +20,11 @@ import VerifyScreen from './screens/VerifyScreen';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Karla-Regular': require('./assets/fonts/Karla/KarlaRegular.ttf'),
+    'Karla-Medium': require('./assets/fonts/Karla/KarlaMedium.ttf'),
+    'Karla-Bold': require('./assets/fonts/Karla/KarlaBold.ttf'),
+  });
   return (
     <RootSiblingParent>
       <NavigationContainer>
@@ -143,6 +152,50 @@ export default function App() {
                   shadowOpacity: 0, // remove shadow on iOS
                 },
                 headerShadowVisible: false,
+                // headerTintColor: '#026E34',
+                headerBackVisible: false,
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Deposit"
+              component={DepositScreen}
+              options={({ navigation }) => ({
+                title: 'Hello John Doe',
+                headerStyle: {
+                  backgroundColor: 'transparent',
+                  fontFamily: 'Karla-Bold',
+                  fontSize: 18,
+                },
+                headerShadowVisible: false,
+                headerTintColor: '#026E34',
+                headerRight: () => (
+                  <Pressable
+                    onPress={() => {
+                      navigation.navigate('NotificationModal');
+                    }}
+                  >
+                    <Ionicons
+                      name="notifications"
+                      size={26}
+                      color={_themeColor.secondary}
+                      style={styles.headerRight}
+                    />
+                  </Pressable>
+                ),
+              })}
+            />
+            <Stack.Screen
+              name="Channel"
+              component={ChannelScreen}
+              options={{
+                title: '',
+                headerStyle: {
+                  backgroundColor: 'transparent',
+                  elevation: 0, // remove shadow on Android
+                  shadowOpacity: 0, // remove shadow on iOS
+                },
+                headerShadowVisible: false,
                 headerTintColor: '#026E34',
                 headerBackVisible: false,
                 headerShown: false,
@@ -150,19 +203,28 @@ export default function App() {
             />
 
             <Stack.Group
-              screenOptions={{
+              screenOptions={({ navigation }) => ({
                 presentation: 'modal',
                 title: 'Notification Area',
-                headerStyle: { display: 'none', fontSize: 20 },
-              }}
+                headerTitleStyle: { fontFamily: 'Karla-Medium' },
+
+                headerRight: () => (
+                  <Pressable
+                    onPress={() => {
+                      navigation.goBack();
+                    }}
+                  >
+                    <Ionicons
+                      name="close-circle"
+                      size={26}
+                      color={_themeColor.gray}
+                      style={styles.headerRight}
+                    />
+                  </Pressable>
+                ),
+              })}
             >
-              <Stack.Screen
-                name="NotificationModal"
-                component={NotificationScreen}
-                option={{
-                  headerStyle: { display: 'none', fontSize: 20 },
-                }}
-              />
+              <Stack.Screen name="NotificationModal" component={NotificationScreen} />
             </Stack.Group>
           </Stack.Group>
         </Stack.Navigator>
