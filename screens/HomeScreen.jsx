6da286f68d5f6625ad358797a09a7a172015534e
iamsatar <abdisatar.mohamed@pixeledge.io>
2023-02-10 02:01:37 +0300

    feat: currency when the fromcurrency and tocurrency are the same

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -164,7 +164,7 @@ export default function HomeScreen() {
                   </Pressable>
                 </View>
 
-                {currency && (
+                {currency && !showToast && (
                   <View style={styles.currencyView}>
                     <Text style={styles.amount}>
                       {parseFloat(currency.toFixed()).toLocaleString()}
