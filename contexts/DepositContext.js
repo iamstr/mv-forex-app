69dd13f89a0ef3496d59cbf7820b9504d98a1c6f
iamsatar <abdisatar.mohamed@pixeledge.io>
2023-02-11 02:08:22 +0300

    chore:create deposit context

---
+++ contexts/DepositContext.js
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useState } from 'react';
+
+export const DepositContext = createContext({
+  deposit: {},
+  saveDeposit: () => {},
+});
+export default function DepositProvider({ children }) {
+  const [deposit, setDeposit] = useState({
+    amount: 0,
+    currency: 'NGN',
+  });
+  useEffect(() => {
+    console.log('The updated deposit value: ', deposit);
+  }, [deposit]);
+  const saveDeposit = (value) => {
+    setDeposit(value);
+  };
+
+  return (
+    <DepositContext.Provider
+      value={{
+        deposit,
+        saveDeposit,
+      }}
+    >
+      {children}
+    </DepositContext.Provider>
+  );
+}
