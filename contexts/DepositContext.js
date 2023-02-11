4ff267fe7f891bccec22e4c741848cbc6fa1a3e7
iamsatar <abdisatar.mohamed@pixeledge.io>
2023-02-12 00:59:44 +0300

    feat: store deposit channel details

--- contexts/DepositContext.js
+++ contexts/DepositContext.js
@@ -3,6 +3,8 @@ import { createContext, useEffect, useState } from 'react';
 export const DepositContext = createContext({
   deposit: {},
   saveDeposit: () => {},
+  channel: {},
+  saveChannel: () => {},
 });
 export default function DepositProvider({ children }) {
   const [deposit, setDeposit] = useState({
@@ -11,11 +13,20 @@ export default function DepositProvider({ children }) {
     from: 'KES',
     exchanged: 0,
   });
+  const [channel, setChannel] = useState({
+    account: 0,
+    label: '',
+    type: '',
+    value: '',
+  });
   useEffect(() => {
-    console.log('The updated deposit value: ', deposit);
-  }, [deposit]);
+    console.log('The updated channel value: ', channel);
+  }, [channel]);
   const saveDeposit = (value) => {
     setDeposit(value);
+  };
+  const saveChannel = (value) => {
+    setChannel(value);
   };
 
   return (
@@ -23,6 +34,8 @@ export default function DepositProvider({ children }) {
       value={{
         deposit,
         saveDeposit,
+        channel,
+        saveChannel,
       }}
     >
       {children}
