f392016e6d4a62bf4cd82ab32d113e2e2c2685ae
iamsatar <abdisatar.mohamed@pixeledge.io>
2023-02-15 00:33:49 +0300

    code:remove unused code

--- screens/SignupScreen.jsx
+++ screens/SignupScreen.jsx
@@ -21,37 +21,6 @@ export default function SignupScreen() {
   const [password, setPassword] = useState(null); // JWT state
   const [confirmPassword, setConfirmPassword] = useState(null); // JWT state
   const navigation = useNavigation();
-  const login = () => {
-    // Send a login request to the Node.js server
-    fetch('https://example.com/login', {
-      method: 'POST',
-      body: JSON.stringify({
-        username: 'user1',
-        password: 'password123',
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Save the JWT locally, such as in the device's local storage
-        saveJWT(data.token);
-        setJWT(data.token);
-      });
-  };
-
-  const getProtectedData = () => {
-    // Make a request to the protected route on the Node.js server
-    const jwt = getJWT(); // Retrieve the JWT from local storage
-    fetch('https://example.com/protected-route', {
-      headers: {
-        Authorization: `Bearer ${jwt}`,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the response data
-        navigation.navigate('Home');
-      });
-  };
 
   return (
     <View>
